58424f397185fbdb3db9fc0b015eea36dcdee3e8
fernando-izar <fizar@me.com>
2022-12-12 17:04:49 -0300

    feat: tarefa1 ok

--- songs/serializers.py
+++ songs/serializers.py
@@ -8,13 +8,3 @@ class SongSerializer(serializers.ModelSerializer):
         model = Song
         fields = ["id", "title", "duration", "album_id"]
         read_only_fields = ["id", "album_id"]
-
-
-# class SongSerializerOld(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(max_length=255)
-#     duration = serializers.CharField(max_length=255)
-#     album_id = serializers.IntegerField(read_only=True)
-
-#     def create(self, validated_data):
-#         return Song.objects.create(**validated_data)
--- users/serializers.py
+++ users/serializers.py
@@ -35,33 +35,3 @@ class UserSerializer(serializers.ModelSerializer):
     #     instance.save()
 
     #     return instance
-
-
-class UserSerializerOld(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    username = serializers.CharField(
-        validators=[
-            UniqueValidator(
-                queryset=User.objects.all(),
-                message="A user with that username already exists.",
-            )
-        ],
-    )
-    email = serializers.EmailField(
-        validators=[UniqueValidator(queryset=User.objects.all())],
-    )
-    password = serializers.CharField(write_only=True)
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    is_superuser = serializers.BooleanField(read_only=True)
-
-    def create(self, validated_data: dict) -> User:
-        return User.objects.create_superuser(**validated_data)
-
-    def update(self, instance: User, validated_data: dict) -> User:
-        for key, value in validated_data.items():
-            setattr(instance, key, value)
-
-        instance.save()
-
-        return instance
